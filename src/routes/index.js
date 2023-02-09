0523c101253d58768129ca1c715b4fe5fff5fdb5
agustin cordoba <cordobajagustin@gmail.com>
2023-02-08 22:57:23 -0300

    rutas

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,7 +9,7 @@ const categories = require("./categories");
 const mercadopago = require("./mercadopago");
 const trademark = require("./trademark");
 const review = require("./review");
-
+const users = require("./user");
 const nodemailer = require("./nodemailer");
 
 const router = Router();
@@ -19,7 +19,7 @@ const router = Router();
 
 router.use("/nmailer", nodemailer);
 router.use("/function2", tryfunction2);
-
+router.use("/users", users);
 router.use("/categories", categories);
 router.use("/products", products);
 //router.use("/function", tryfunction);
