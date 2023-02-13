769358555518887542f503c751f5b97ef2a22209
Fatima Ponce <faatii.ponce@gmail.com>
2023-02-13 12:25:41 -0300

    delte

--- src/routes/deleteproduct.js
+++ src/routes/deleteproduct.js
@@ -1,21 +1,20 @@
 const { Products, Category } = require("../db"); //database
 const { Router } = require("express");
 const router = Router();
-const { Sequelize, ARRAY, DatabaseError } = require('sequelize');
-const { deleteproduct } = require('../controllers/cart')
+const { Sequelize, ARRAY, DatabaseError } = require("sequelize");
+const { deleteproduct } = require("../controllers/cart");
 
-router.post('/', async (req, res) => {
-    console.log("ENTRANDO A ROUTER.POST DELETE_PRODUCT")
-    console.log("body es...")
-    console.log(req.body)
-    const {userid, idproduct, quantity} = req.body
-    try {
-        let response = await deleteproduct(userid, idproduct, quantity)
-        res.status(201).send(response)
-    } catch (error) {
-        res.status(404).send(error.message)
-    }
-})
-
+router.post("/", async (req, res) => {
+  console.log("ENTRANDO A ROUTER.POST DELETE_PRODUCT");
+  console.log("body es...");
+  console.log(req.body);
+  const { userid, idproduct, quantity } = req.body;
+  try {
+    let response = await deleteproduct(userid, idproduct, quantity);
+    res.status(201).send(response);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
 
 module.exports = router;
